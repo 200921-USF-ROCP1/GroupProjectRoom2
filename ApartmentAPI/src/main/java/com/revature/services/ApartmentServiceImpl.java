2cffd8b873673e2dff97fe75c49b759843a14bf1
mtwhite4 <47282084+mtwhite4@users.noreply.github.com>
2020-10-05 15:56:01 -0400

    minor DAO changes

--- ApartmentAPI/src/main/java/com/revature/services/ApartmentServiceImpl.java
+++ ApartmentAPI/src/main/java/com/revature/services/ApartmentServiceImpl.java
@@ -1,17 +1,23 @@
 package com.revature.services;
 
+import java.util.Scanner;
+
+import com.revature.app.App;
+import com.revature.dao.ApartmentDAO;
 import com.revature.interfaces.ApartmentService;
+import com.revature.interfaces.DAO;
 
 public class ApartmentServiceImpl implements ApartmentService{
+	private Scanner sc = App.sc;
+	private ApartmentDAO aDAO = new ApartmentDAO();
 
 	public void getEmptyApartments() {
-		// TODO Auto-generated method stub
-		
+		aDAO.getVacantApatments();
+
 	}
 
 	public void getApartmentPets() {
-		// TODO Auto-generated method stub
-		
+		aDAO.getVacantApatments();
 	}
 
 	public void getIncome() {
