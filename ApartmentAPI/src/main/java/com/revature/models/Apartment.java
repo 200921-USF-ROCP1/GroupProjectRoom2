da2ed3a7926b9583b410d33f2cbe1271096d9249
kagorcp <christinepenza@live.com>
2020-10-05 15:45:24 -0400

    Apartment Model

--- ApartmentAPI/src/main/java/com/revature/models/Apartment.java
+++ ApartmentAPI/src/main/java/com/revature/models/Apartment.java
@@ -1,6 +1,7 @@
 package com.revature.models;
 
 public class Apartment {
+<<<<<<< HEAD
 	private int id;
 	private char buildingLetter;
 	private int roomNumber;
@@ -33,4 +34,7 @@ public class Apartment {
 	public void setMonthlyRent(double monthlyRent) {
 		this.monthlyRent = monthlyRent;
 	}
+=======
+	
+>>>>>>> f94deaf76f74f06e543c62d4b19202ee8eef3474
 }
