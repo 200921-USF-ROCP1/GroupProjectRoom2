0de6c63f4eaf14318b58206fc3d1ee904a727be2
mtwhite4 <47282084+mtwhite4@users.noreply.github.com>
2020-10-05 17:05:12 -0400

    errors

--- ApartmentAPI/src/main/java/com/revature/dao/ResidentDAO.java
+++ ApartmentAPI/src/main/java/com/revature/dao/ResidentDAO.java
@@ -3,6 +3,7 @@ package com.revature.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import com.revature.interfaces.DAO;
 import com.revature.models.Resident;
@@ -17,12 +18,12 @@ public class ResidentDAO implements DAO<Resident> {
 		connection = ConnectionService.getConnection();
 	}
 	
-	public void create(Resident t) {
+	public void create(Resident t) throws SQLException {
 		ps = connection.prepareStatement("insert into resident(iD,fname,lname,aptID) values (?,?,?,?)");
 		ps.setInt(1, t.getId());
 		ps.setString(2, t.getFirstName());
 		ps.setString(3, t.getLastName());
-		ps.setInt(4, t.getApartment_id())
+		ps.setInt(4, t.getApartment_id());
 		
 		ps.execute();
 		
