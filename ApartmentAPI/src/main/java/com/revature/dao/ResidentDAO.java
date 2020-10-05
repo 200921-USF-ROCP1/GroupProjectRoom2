60c954b3cf76acdd3c79c771fe813c9ec91f02ee
Alec-Bowald <Aleczandr.bowald.16@cnu.edu>
2020-10-05 16:59:49 -0400

    Implemented residentsDAO create

--- ApartmentAPI/src/main/java/com/revature/dao/ResidentDAO.java
+++ ApartmentAPI/src/main/java/com/revature/dao/ResidentDAO.java
@@ -4,9 +4,23 @@ import com.revature.interfaces.DAO;
 import com.revature.models.Resident;
 
 public class ResidentDAO implements DAO<Resident> {
-
+	PreparedStatement ps;
+	ResultSet rs;
+	Connection connection;
+	
+	public residentDAO() { //constructor
+		connection = ConnectionService.getConnection();
+	}
+	
 	public void create(Resident t) {
-		// TODO Auto-generated method stub
+		ps = connection.prepareStatement("insert into resident(iD,fname,lname,aptID) values (?,?,?,?)");
+		ps.setInt(1, t.getId());
+		ps.setString(2, t.getFirstName());
+		ps.setString(3, t.getLastName());
+		ps.setInt(4, t.getApartment_id())
+		
+		ps.execute();
+		
 		
 	}
 
