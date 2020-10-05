e1769ef3e2fcd5a7a2ef2b0a6a5e62c47f80e58d
kagorcp <christinepenza@live.com>
2020-10-05 15:29:59 -0400

    Apartment Model

--- ApartmentAPI/src/main/java/com/revature/models/Apartment.java
+++ ApartmentAPI/src/main/java/com/revature/models/Apartment.java
@@ -1,5 +1,36 @@
 package com.revature.models;
 
 public class Apartment {
-
+	private int id;
+	private char buildingLetter;
+	private int roomNumber;
+	private double monthlyRent;
+	
+	public Apartment( char buildingLetter, int roomNumber, double MonthlyRent ) {
+		this.setBuildingLetter(buildingLetter);
+		this.setRoomNumber(roomNumber);
+		this.setMonthlyRent(monthlyRent);
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public char getbuildingLetter() {
+		return buildingLetter;
+	}
+	public void setBuildingLetter(char buildingLetter) {
+		this.buildingLetter = buildingLetter;
+	}
+	public int getroomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(int roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public double getMonthlyRent() {
+		return monthlyRent;
+	}
+	public void setMonthlyRent(double monthlyRent) {
+		this.monthlyRent = monthlyRent;
+	}
 }
